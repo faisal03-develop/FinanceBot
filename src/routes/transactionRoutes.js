b1576967f88d07e85f99c89f7b9816e15988b822
Faisal Altaf <altaffaisal532@gmail.com>
2026-05-06 14:15:41 +0500

    feat: add transaction routes for parsing and retrieving data

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { processTransaction } = require('../controllers/transactionController');
+const { processTransaction, getTransactions } = require('../controllers/transactionController');
 
 router.post('/parse', processTransaction);
+router.get('/', getTransactions);
+
 
 module.exports = router;
