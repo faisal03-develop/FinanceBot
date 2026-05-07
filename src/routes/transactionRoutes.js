b33fdcea439c25cacff77f99be46cb7d11432332
Faisal Altaf <altaffaisal532@gmail.com>
2026-05-07 11:53:17 +0500

    refactor: remove legacy files and services related to transaction processing and Firebase integration

--- src/routes/transactionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { processTransaction, getTransactions } = require('../controllers/transactionController');
-
-router.post('/parse', processTransaction);
-router.get('/', getTransactions);
-
-
-module.exports = router;
